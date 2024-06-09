6490c100d6f856bcf30ae46ae46830f989c7a4e0
isadadi <idadi70@gmail.com>
2024-06-09 13:11:19 +0700

    config secret_key_base

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,5 +88,5 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   config.hosts << ENV['DEVELOPMENT_HOSTS']
-  config.secret_key_base = Rails.application.secret_key_base
+  config.secret_key_base = ENV['SECRET_KEY_BASE']
 end
